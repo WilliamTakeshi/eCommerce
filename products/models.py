b64dc929c10364858a94cc6a80ef4ea25b613d2b
Will <williamtpereira@gmail.com>
2018-04-17 14:04:26 -0300

    added slugfield in product model and sluggenerator in signals

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import os
 import random
+from django.db.models.signals import pre_save
+from .utils import unique_slug_generator
 # Create your models here.
 
 
@@ -39,6 +41,7 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
@@ -49,3 +52,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
